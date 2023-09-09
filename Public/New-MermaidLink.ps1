4bdfff34075656bd3343060d9c1d67ae43f413fc
Christian Ritter <h.christian.ritter@gmail.com>
2023-09-09 13:58:23 +0200

    Update New-MermaidLink.ps1
    Added functionality to 'New-MermaidLink' to generate Bidirectional links, added new arrow types (Arrow, Dot and Cross).

--- Public/New-MermaidLink.ps1
+++ Public/New-MermaidLink.ps1
@@ -1,30 +1,55 @@
 class LinkType : System.Management.Automation.IValidateSetValuesGenerator {
     [String[]] GetValidValues() {
         $Global:LinkTypes = @{ 
-            'OpenLink' = '---'
-            'ArrowLink' = '-->'
-            'DottedLink' = '-.->'
-            'ThickLink' = '==>'
-            'InvisibleLink' = '~~~' 
+            'Link'      = '---'
+            'Thick'     = '==='
+            'Dotted'    = '-.-'
+            'Invisible' = '~~~' 
         }
         return ($Global:LinkTypes).Keys
     }
 }
+class ArrowType : System.Management.Automation.IValidateSetValuesGenerator {
+    [String[]] GetValidValues() {
+        $Global:ArrowTypes = @{ 
+            'Dot'      = 'o'
+            'Cross'    = 'x'
+            'Arrow'    = '>'
+        }
+        return ($Global:ArrowTypes).Keys
+    }
+}
+
 
 function New-MermaidLink {
     [CmdletBinding()]
     param (
         $Text ='',
         [ValidateSet([LinkType],ErrorMessage="Value '{0}' is invalid. Try one of: {1}")]
-        $Linktype = 'ArrowLink'
+        $Linktype = 'LinkType',
+        [ValidateSet([ArrowType],ErrorMessage="Value '{0}' is invalid. Try one of: {1}")]
+        $ArrowType = $null,
+        [switch] $BiDirectional
     )
     
     begin {
         $ReturnString = ''
         $SelectedLinkType = $Global:LinkTypes[$Linktype]
+        if(-not([string]::IsNullOrEmpty($ArrowType))){
+            $SelectedArrowType = $Global:ArrowTypes[$Arrowtype]
+            $SelectedLinkType += $SelectedArrowType
+        }
     }
     
     process {
+        if($BiDirectional -and (-not([string]::IsNullOrEmpty($ArrowType)))){
+            if($ArrowType -eq "Arrow"){
+                $SelectedLinkType = $SelectedLinkType -replace '^(.)', '<'
+            }else{
+                $SelectedLinkType = "$SelectedArrowType$SelectedLinkType"
+            }
+        }
+
         if(-not [string]::IsNullOrEmpty($Text)){
             $Text = "|$Text|"
         }
